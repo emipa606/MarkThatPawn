5b09c4edc8f976a82f9112a698df69de0d3ef4aa
Mlie <emipa606@users.noreply.github.com>
2024-02-18 19:55:16 +0100

    Fixed issue with newly created TD Find rules only being editable after reload of save

--- Source/TDFindLibRule/TDFindLibRule.cs
+++ Source/TDFindLibRule/TDFindLibRule.cs
@@ -24,6 +24,7 @@ public class TDFindLibRule : MarkerRule
         }
 
         Current.Game.GetComponent<GameComponent_TDFindLibRuleComponent>().TDFindLibSearches.Add(tdSearcher);
+        IsInCorrectGame = true;
     }
 
     public TDFindLibRule(string blob)
